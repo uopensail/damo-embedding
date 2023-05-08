04569dd5bbc769e68be8cf4e0f12490facd36809
kungjim <7629149+kungjim@users.noreply.github.com>
2023-05-08 22:12:05 +0800

    add build scripts

---
+++ scripts/prepare_build_debain.sh
@@ -0,0 +1,23 @@
+#!/bin/bash -x
+
+apt-get install -y python-dev wget
+
+# Build Rocksdb
+apt-get install -y libgflags-dev
+apt-get install -y libsnappy-dev zlib1g-dev libbz2-dev liblz4-dev libzstd-dev
+
+# Build With Source
+
+cd /tmp
+wget https://github.com/facebook/rocksdb/archive/v6.4.6.tar.gz
+tar -xvzf v6.4.6.tar.gz
+cd rocksdb-6.4.6/
+mkdir build
+cd build
+cmake -DCMAKE_INSTALL_PREFIX=/usr/local/rocksdb ..
+
+# online env NOT USE make/make all
+# online env make static_lib/make shared_lib
+# please refer to the official documentation for details
+make shared_lib EXTRA_CXXFLAGS=-fPIC EXTRA_CFLAGS=-fPIC USE_RTTI=1 DEBUG_LEVEL=0 
+make install-shared
