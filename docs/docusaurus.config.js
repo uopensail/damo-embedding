d02625c02e01cacc7a1e8de7cd2d6edc5b7cb09c
kungjim <7629149+kungjim@users.noreply.github.com>
2023-05-08 23:25:35 +0800

    update doc website

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -48,14 +48,14 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/uopensail/damo-embedding/edit/docs/docs',
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/uopensail/damo-embedding/edit/docs/docs',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
