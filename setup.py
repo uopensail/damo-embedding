ca974bf7e2431e5a57a38fdce06c3f2ea2b1966c
ulifedeloy <65488844+ulifedeloy@users.noreply.github.com>
2024-01-15 16:40:49 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -180,7 +180,7 @@ with open("README.md", "r", encoding="utf-8") as fd:
 
 setup(
     name="damo-embedding",
-    version="1.1.7",
+    version="1.1.8",
     description="Python wrapper for damo, a set of fast and robust hash functions.",
     license="License :: AGLP3",
     author="timepi",
